e6aa591fd35550b82d23325b9d658712fc0f7bb1
Simon-mac <zeroing_one@163.com>
2019-03-21 13:32:46 +0800

    更新事件回调的demo

--- eventsCallback/server.php
+++ eventsCallback/server.php
@@ -36,14 +36,15 @@ $serv->on('WorkerStart', function (Swoole\Server $server, int $worker_id) {
         ($server->taskworker === true ?
             'taskWorker进程PID为' . $server->worker_pid :
             'Worker进程PID为' . $server->worker_pid) . PHP_EOL;
-    // 使用php的Fatal Error模拟进程异常退出的错误
+    // 使用php的Fatal Error模拟进程异常退出的错误，进程出错就可以触发WorkerError事件回调
+    // 一般情况都会在业务代码使用try catch捕获异常，防止进程异常重启
+    // 使用命令：php server.php >> test.txt   将命令行打印的信息输出到文本文件中查看
     // new fsfsdfds();
 });
 // worker/taskWorker进程异常触发的回调
 $serv->on('WorkerError', function (Swoole\Server $server, int $worker_id) {
     echo '触发了WorkerError事件回调' . PHP_EOL;
 });
-//
 $serv->on('WorkerExit', function (Swoole\Server $server) {
     echo '触发了WorkerExit事件回调' . PHP_EOL;
 });
@@ -70,7 +71,8 @@ $serv->on('Finish', function (Swoole\Server $server, int $task_id, string $data)
 $serv->on('Connect', function (Swoole\Server $server, int $fd, int $reactorId) {
     echo '触发了Connect事件回调' . PHP_EOL;
 });
-// 收到客户端发送过来的数据时触发的回调
+// 收到客户端发送过来的数据时触发的回调，
+//如果启动的是udp服务，监听的是onPacket事件
 $serv->on('Receive', function (Swoole\Server $server, int $fd, int $reactor_id, string $data) {
     echo '触发了Receive事件回调' . PHP_EOL;
     $server->task('发送给taskWorker进程的数据');
